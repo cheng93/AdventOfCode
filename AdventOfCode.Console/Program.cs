bcfb406dab787ac1858473d1cc2e8677639e4cb6
cheng93 <derek.c@hotmail.co.uk>
2023-11-26 11:31:40 +0000

    Start of 2023

--- AdventOfCode.Console/Program.cs
+++ AdventOfCode.Console/Program.cs
@@ -1,69 +1,70 @@
-﻿namespace AdventOfCode.Console
+﻿namespace AdventOfCode.Console;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AdventOfCode.Abstractions;
+using AdventOfCode2016;
+using AdventOfCode2017;
+using AdventOfCode2018;
+using AdventOfCode2019;
+using AdventOfCode2020;
+using AdventOfCode2021;
+using AdventOfCode2022;
+using AdventOfCode2023;
+using CommandLine;
+using Console = System.Console;
+
+class Program
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Threading.Tasks;
-    using AdventOfCode.Abstractions;
-    using AdventOfCode2016;
-    using AdventOfCode2017;
-    using AdventOfCode2018;
-    using AdventOfCode2019;
-    using AdventOfCode2020;
-    using AdventOfCode2021;
-    using AdventOfCode2022;
-    using CommandLine;
-    using Console = System.Console;
-
-    class Program
+    private static IDictionary<int, IPuzzleFactory> puzzleFactories = new Dictionary<int, IPuzzleFactory>()
     {
-        private static IDictionary<int, IPuzzleFactory> puzzleFactories = new Dictionary<int, IPuzzleFactory>()
-        {
-            {2016, new Year2016PuzzleFactory()},
-            {2017, new Year2017PuzzleFactory()},
-            {2018, new Year2018PuzzleFactory()},
-            {2019, new Year2019PuzzleFactory()},
-            {2020, new Year2020PuzzleFactory()},
-            {2021, new Year2021PuzzleFactory()},
-            {2022, new Year2022PuzzleFactory()}
-        };
+        {2016, new Year2016PuzzleFactory()},
+        {2017, new Year2017PuzzleFactory()},
+        {2018, new Year2018PuzzleFactory()},
+        {2019, new Year2019PuzzleFactory()},
+        {2020, new Year2020PuzzleFactory()},
+        {2021, new Year2021PuzzleFactory()},
+        {2022, new Year2022PuzzleFactory()},
+        {2023, new Year2023PuzzleFactory()}
+    };
 
-        public static async Task Main(string[] args)
-        {
+    public static async Task Main(string[] args)
+    {
 
-            var parsed = Parser.Default.ParseArguments<ConsoleOptions>(args);
+        var parsed = Parser.Default.ParseArguments<ConsoleOptions>(args);
 
-            await parsed.MapResult(
-                async options =>
+        await parsed.MapResult(
+            async options =>
+            {
+                if (options.Debug)
                 {
-                    if (options.Debug)
-                    {
-                        Console.WriteLine("Debugging...");
-                        Console.ReadLine();
-                    }
-                    var puzzle = puzzleFactories[options.Year].Create(options.Day);
+                    Console.WriteLine("Debugging...");
+                    Console.ReadLine();
+                }
+                var puzzle = puzzleFactories[options.Year].Create(options.Day);
 
-                    await Execute(() => puzzle.PuzzleOne());
-                    await Execute(() => puzzle.PuzzleTwo());
-                },
-                errors => Task.CompletedTask);
+                await Execute(() => puzzle.PuzzleOne());
+                await Execute(() => puzzle.PuzzleTwo());
+            },
+            errors => Task.CompletedTask);
 
-            Console.ReadLine();
-        }
+        Console.ReadLine();
+    }
 
-        private static async Task Execute(Func<Task<string>> solution)
+    private static async Task Execute(Func<Task<string>> solution)
+    {
+        var watch = Stopwatch.StartNew();
+        try
         {
-            var watch = Stopwatch.StartNew();
-            try
-            {
-                Console.WriteLine(await solution());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            watch.Stop();
-            Console.WriteLine($"Time taken: {watch.ElapsedMilliseconds}ms");
+            Console.WriteLine(await solution());
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        watch.Stop();
+        Console.WriteLine($"Time taken: {watch.ElapsedMilliseconds}ms");
     }
 }
---
+++ Solutions/2023/AdventOfCode2023/Year2023PuzzleFactory.cs
@@ -0,0 +1,61 @@
+﻿// using AdventOfCode2023.Day01;
+// using AdventOfCode2023.Day02;
+// using AdventOfCode2023.Day03;
+// using AdventOfCode2023.Day04;
+// using AdventOfCode2023.Day05;
+// using AdventOfCode2023.Day06;
+// using AdventOfCode2023.Day07;
+// using AdventOfCode2023.Day08;
+// using AdventOfCode2023.Day09;
+// using AdventOfCode2023.Day10;
+// using AdventOfCode2023.Day11;
+// using AdventOfCode2023.Day12;
+// using AdventOfCode2023.Day13;
+// using AdventOfCode2023.Day14;
+// using AdventOfCode2023.Day15;
+// using AdventOfCode2023.Day16;
+// using AdventOfCode2023.Day17;
+// using AdventOfCode2023.Day18;
+// using AdventOfCode2023.Day19;
+// using AdventOfCode2023.Day20;
+// using AdventOfCode2023.Day21;
+// using AdventOfCode2023.Day22;
+// using AdventOfCode2023.Day23;
+// using AdventOfCode2023.Day24;
+// using AdventOfCode2023.Day25;
+
+namespace AdventOfCode2023;
+
+public class Year2023PuzzleFactory : IPuzzleFactory
+{
+    public IPuzzle Create(int day)
+        => day switch
+        {
+            // 1 => new Day01Puzzle(),
+            // 2 => new Day02Puzzle(),
+            // 3 => new Day03Puzzle(),
+            // 4 => new Day04Puzzle(),
+            // 5 => new Day05Puzzle(),
+            // 6 => new Day06Puzzle(),
+            // 7 => new Day07Puzzle(),
+            // 8 => new Day08Puzzle(),
+            // 9 => new Day09Puzzle(),
+            // 10 => new Day10Puzzle(),
+            // 11 => new Day11Puzzle(),
+            // 12 => new Day12Puzzle(),
+            // 13 => new Day13Puzzle(),
+            // 14 => new Day14Puzzle(),
+            // 15 => new Day15Puzzle(),
+            // 16 => new Day16Puzzle(),
+            // 17 => new Day17Puzzle(),
+            // 18 => new Day18Puzzle(),
+            // 19 => new Day19Puzzle(),
+            // 20 => new Day20Puzzle(),
+            // 21 => new Day21Puzzle(),
+            // 22 => new Day22Puzzle(),
+            // 23 => new Day23Puzzle(),
+            // 24 => new Day24Puzzle(),
+            // 25 => new Day25Puzzle(),
+            _ => throw new ArgumentException(nameof(day))
+        };
+}
